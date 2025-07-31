cd25a2e23d704e07acde5b5554503933ad484777
iyxi <iyannaangela.marquez@gmail.com>
2025-07-31 15:40:23 +0800

    hello

--- FUNDACOMPROG PROJ/Form2.Designer.vb
+++ FUNDACOMPROG PROJ/Form2.Designer.vb
@@ -31,38 +31,42 @@ Partial Class Form2
         '
         Me.Button1.BackColor = System.Drawing.Color.Transparent
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
-        Me.Button1.Location = New System.Drawing.Point(417, 453)
+        Me.Button1.Location = New System.Drawing.Point(371, 362)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(196, 40)
+        Me.Button1.Size = New System.Drawing.Size(174, 32)
         Me.Button1.TabIndex = 0
         Me.Button1.UseVisualStyleBackColor = False
         '
         'TextBox1
         '
         Me.TextBox1.BackColor = System.Drawing.SystemColors.InactiveBorder
-        Me.TextBox1.Location = New System.Drawing.Point(417, 269)
+        Me.TextBox1.Location = New System.Drawing.Point(371, 215)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(320, 26)
+        Me.TextBox1.Size = New System.Drawing.Size(285, 22)
         Me.TextBox1.TabIndex = 1
         '
         'TextBox2
         '
         Me.TextBox2.BackColor = System.Drawing.SystemColors.InactiveBorder
-        Me.TextBox2.Location = New System.Drawing.Point(417, 356)
+        Me.TextBox2.Location = New System.Drawing.Point(371, 285)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(320, 26)
+        Me.TextBox2.Size = New System.Drawing.Size(285, 22)
         Me.TextBox2.TabIndex = 2
         '
         'Form2
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.FUNDACOMPROG_PROJ.My.Resources.Resources.really_great_site__2_
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(1033, 630)
+        Me.ClientSize = New System.Drawing.Size(918, 504)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Form2"
         Me.ResumeLayout(False)
         Me.PerformLayout()
--- FUNDACOMPROG PROJ/Form2.vb
+++ FUNDACOMPROG PROJ/Form2.vb
@@ -6,4 +6,8 @@
     Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'hello luis
+    End Sub
 End Class
