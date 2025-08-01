6cca87bdf467e0fd0ca27f0bef159411633cc67f
iyxi <iyannaangela.marquez@gmail.com>
2025-08-01 13:08:06 +0800

    inventory

---
+++ FUNDACOMPROG PROJ/Form15.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form15
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(554, 418)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(47, 17)
+        Me.Label5.TabIndex = 57
+        Me.Label5.Text = "ItemID"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(435, 418)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox4.TabIndex = 56
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(769, 198)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 55
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(769, 151)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 54
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(769, 102)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 53
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(552, 119)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(83, 17)
+        Me.Label8.TabIndex = 52
+        Me.Label8.Text = "CostPerUnit"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(657, 242)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(102, 17)
+        Me.Label7.TabIndex = 51
+        Me.Label7.Text = "LastRestocked"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(574, 296)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(33, 17)
+        Me.Label4.TabIndex = 50
+        Me.Label4.Text = "Unit"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(574, 178)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 17)
+        Me.Label3.TabIndex = 49
+        Me.Label3.Text = "Quantity"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(552, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(71, 17)
+        Me.Label2.TabIndex = 48
+        Me.Label2.Text = "ItemName"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(57, 40)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(356, 368)
+        Me.DataGridView1.TabIndex = 47
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(435, 237)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 22)
+        Me.DateTimePicker1.TabIndex = 46
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(435, 357)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 24)
+        Me.ComboBox2.TabIndex = 45
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(435, 296)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 24)
+        Me.ComboBox1.TabIndex = 44
+        Me.ComboBox1.Text = "`"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(435, 178)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 22)
+        Me.NumericUpDown1.TabIndex = 43
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(435, 119)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox3.TabIndex = 42
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(435, 60)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 41
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(574, 357)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 17)
+        Me.Label1.TabIndex = 40
+        Me.Label1.Text = "UserID"
+        '
+        'Form15
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(918, 504)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form15"
+        Me.Text = "Form15"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ FUNDACOMPROG PROJ/Form15.vb
@@ -0,0 +1,155 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form15
+    Dim conn As New MySqlConnection("server=localhost;user id=root;password=;database=loadlog")
+
+    Private Sub Form15_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadInventory()
+        LoadUsers()
+        LoadUnits()
+    End Sub
+
+    Private Sub LoadInventory()
+        Try
+            Dim cmd As New MySqlCommand("SELECT * FROM inventory", conn)
+            Dim adapter As New MySqlDataAdapter(cmd)
+            Dim dt As New DataTable()
+            adapter.Fill(dt)
+            DataGridView1.DataSource = dt
+        Catch ex As Exception
+            MessageBox.Show("Error loading inventory: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub LoadUsers()
+        Try
+            Dim cmd As New MySqlCommand("SELECT UserID FROM users", conn)
+            Dim adapter As New MySqlDataAdapter(cmd)
+            Dim dt As New DataTable()
+            adapter.Fill(dt)
+            ComboBox2.DataSource = dt
+            ComboBox2.DisplayMember = "UserID"
+            ComboBox2.ValueMember = "UserID"
+        Catch ex As Exception
+            MessageBox.Show("Error loading users: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub LoadUnits()
+        ComboBox1.Items.Clear()
+        ComboBox1.Items.AddRange(New String() {"pcs", "kg", "liters", "packs"})
+        ComboBox1.SelectedIndex = 0
+    End Sub
+
+    ' INSERT
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim itemName As String = TextBox1.Text.Trim()
+        Dim quantity As Integer = NumericUpDown1.Value
+        Dim unit As String = ComboBox1.Text.Trim()
+        Dim lastRestocked As String = DateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm:ss")
+        Dim costPerUnitStr As String = TextBox3.Text.Trim()
+        Dim userID As String = ComboBox2.Text.Trim()
+
+        If itemName = "" Then
+            MessageBox.Show("Item Name is required.")
+            Return
+        ElseIf unit = "" Then
+            MessageBox.Show("Unit is required.")
+            Return
+        ElseIf userID = "" Then
+            MessageBox.Show("UserID is required.")
+            Return
+        End If
+
+        Dim restockedPart As String = If(DateTimePicker1.Checked, "'" & lastRestocked & "'", "NULL")
+        Dim costPart As String = If(costPerUnitStr = "", "NULL", costPerUnitStr)
+
+        Dim query As String = "INSERT INTO inventory (ItemName, Quantity, Unit, LastRestocked, CostPerUnit, UserID) VALUES (" &
+                              "'" & itemName.Replace("'", "''") & "', " & quantity & ", '" & unit.Replace("'", "''") & "', " &
+                              restockedPart & ", " & costPart & ", " & userID & ")"
+
+        Try
+            Dim cmd As New MySqlCommand(query, conn)
+            conn.Open()
+            cmd.ExecuteNonQuery()
+            conn.Close()
+            MessageBox.Show("Item inserted.")
+            LoadInventory()
+        Catch ex As Exception
+            MessageBox.Show("Insert error: " & ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+
+    ' UPDATE
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim itemID As String = TextBox4.Text.Trim()
+        Dim itemName As String = TextBox1.Text.Trim()
+        Dim quantity As Integer = NumericUpDown1.Value
+        Dim unit As String = ComboBox1.Text.Trim()
+        Dim lastRestocked As String = DateTimePicker1.Value.ToString("yyyy-MM-dd HH:mm:ss")
+        Dim costPerUnitStr As String = TextBox3.Text.Trim()
+        Dim userID As String = ComboBox2.Text.Trim()
+
+        If itemID = "" Then
+            MessageBox.Show("Enter ItemID to update.")
+            Return
+        End If
+
+        Dim restockedPart As String = If(DateTimePicker1.Checked, "'" & lastRestocked & "'", "NULL")
+        Dim costPart As String = If(costPerUnitStr = "", "NULL", costPerUnitStr)
+
+        Dim query As String = "UPDATE inventory SET " &
+                              "ItemName = '" & itemName.Replace("'", "''") & "', " &
+                              "Quantity = " & quantity & ", " &
+                              "Unit = '" & unit.Replace("'", "''") & "', " &
+                              "LastRestocked = " & restockedPart & ", " &
+                              "CostPerUnit = " & costPart & ", " &
+                              "UserID = " & userID & " " &
+                              "WHERE ItemID = " & itemID
+
+        Try
+            Dim cmd As New MySqlCommand(query, conn)
+            conn.Open()
+            Dim rows = cmd.ExecuteNonQuery()
+            conn.Close()
+            If rows > 0 Then
+                MessageBox.Show("Item updated.")
+                LoadInventory()
+            Else
+                MessageBox.Show("ItemID not found.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Update error: " & ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+
+    ' DELETE
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim itemID As String = TextBox4.Text.Trim()
+
+        If itemID = "" Then
+            MessageBox.Show("Enter ItemID to delete.")
+            Return
+        End If
+
+        Dim query As String = "DELETE FROM inventory WHERE ItemID = " & itemID
+
+        Try
+            Dim cmd As New MySqlCommand(query, conn)
+            conn.Open()
+            Dim rows = cmd.ExecuteNonQuery()
+            conn.Close()
+            If rows > 0 Then
+                MessageBox.Show("Item deleted.")
+                LoadInventory()
+            Else
+                MessageBox.Show("ItemID not found.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Delete error: " & ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+End Class
